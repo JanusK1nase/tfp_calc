07a2290b87a765d255e6659dfdc0abddf76d549c
JanusK1nase <reginaldtabora@gmail.com>
2025-02-05 18:27:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -601,8 +601,8 @@ def lidocaine():
         fluidrate = st.number_input("Please indicate patient's current fluid rate: ")
         crigive = (0.12 / fluidrate * 1000 / 20)
         print = (f"Lidocaine 20mg/ml: \nGive {firstgive:.2f} ml IV ONCE. \nRemove {crigive:.1f} ml from fluid bottle then add {crigive:.1f} ml of Lidocaine. \nFluid Rate: {fluidrate} ml/hr")
-        st.write print
-        return (print)
+        st.write (print)
+        return print
 
     
 
